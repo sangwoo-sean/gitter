c5ce5e32f0c12cfcdeec4a6e4bbf9e6b57ed86cc
sangwoo <tkddn3367@gmail.com>
2023-05-08 23:31:16 +0900

    introduce styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,30 @@
 import "./App.css";
+import styled from "styled-components";
 
 function App() {
-  return <></>;
+  class Commit {
+    id: string;
+    message?: string;
+
+    constructor(id: string, message?: string) {
+      this.id = id;
+      this.message = message;
+    }
+  }
+
+  return (
+    <AppWrapper>
+      <h1>Hello</h1>
+      <input type="text" />
+    </AppWrapper>
+  );
 }
+
+const AppWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+`;
 
 export default App;
