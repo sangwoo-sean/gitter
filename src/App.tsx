f80b068d88b72a685b25c8b1a32931eeff0125a6
sangwoo <tkddn3367@gmail.com>
2023-05-08 23:44:05 +0900

    get command and add to history list on enter input

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 import "./App.css";
 import styled from "styled-components";
+import { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState<string>("");
+  const [history, setHistory] = useState<Array<string>>([]);
+
   class Commit {
     id: string;
     message?: string;
@@ -12,10 +16,26 @@ function App() {
     }
   }
 
+  function onChange(event: React.ChangeEvent<HTMLInputElement>) {
+    setInput(event.target.value);
+  }
+
+  function onKeyDown(event: React.KeyboardEvent<HTMLInputElement>) {
+    if (event.key === "Enter") {
+      setHistory((history) => [...history, input]);
+      setInput("");
+    }
+  }
+
   return (
     <AppWrapper>
       <h1>Hello</h1>
-      <input type="text" />
+      <ul>
+        {history.map((command) => (
+          <li>{command}</li>
+        ))}
+      </ul>
+      <input type="text" value={input} onChange={onChange} onKeyDown={onKeyDown} />
     </AppWrapper>
   );
 }
