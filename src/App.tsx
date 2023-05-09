e177bcb2b50a75a37dd73f8195fdabbf2075fc80
sangwoo <tkddn3367@gmail.com>
2023-05-09 22:55:49 +0900

    add commit node css

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ function App() {
   const [input, setInput] = useState<string>("");
   const [history, setHistory] = useState<Array<string>>([]);
   const [isValidCommand, setIsValidCommand] = useState<boolean>(true);
+  const [count, setCount] = useState<number>(1);
 
   function onChange(event: React.ChangeEvent<HTMLInputElement>) {
     setInput(event.target.value);
@@ -28,22 +29,39 @@ function App() {
   return (
     <AppWrapper>
       <h1>Hello</h1>
-      <ul>
-        {history.map((command, index) => (
-          <li key={index}>{command}</li>
-        ))}
-      </ul>
-      <input type="text" value={input} onChange={onChange} onKeyDown={onKeyDown} />
-      <ErrorText style={{ display: isValidCommand ? "none" : "inline" }}>올바르지 않은 명령어입니다.</ErrorText>
+      <div>
+        <div className="commit">
+          <span>4</span>
+        </div>
+        <div className="commit">
+          <span>3</span>
+        </div>
+        <div className="commit">
+          <span>2</span>
+        </div>
+        <div className="commit">
+          <span>1</span>
+        </div>
+      </div>
+      <div>
+        <ul>
+          {history.map((command, index) => (
+            <li key={index}>{command}</li>
+          ))}
+        </ul>
+        <input type="text" value={input} onChange={onChange} onKeyDown={onKeyDown} />
+        <ErrorText style={{ display: isValidCommand ? "none" : "inline" }}>올바르지 않은 명령어입니다.</ErrorText>
+      </div>
     </AppWrapper>
   );
 }
 
 const AppWrapper = styled.div`
   display: flex;
-  justify-content: center;
+  justify-content: space-between;
   flex-direction: column;
   align-items: center;
+  height: 90vh;
 `;
 
 const ErrorText = styled.span`
