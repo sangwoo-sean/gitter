64cf3fb10a492cdc4ef378e73b35c0c55b8853ae
sangwoo <tkddn3367@gmail.com>
2023-05-19 16:54:59 +0900

    ver2 init

---
+++ src/App2.tsx
@@ -0,0 +1,7 @@
+export default function App2() {
+  return (
+    <>
+      <h1>thisisApp2</h1>
+    </>
+  );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+import App2 from "./App2.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <App2 />
   </React.StrictMode>
 );
